a4c6ee15c3c12ccbaa1c2f0be4638c198f7ed439
ThHutterer <th.hutterer@gmail.com>
2021-04-29 18:47:02 +0200

    attempt to run on sense hat

--- game_of_life.py
+++ game_of_life.py
@@ -1,14 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+from scipy.ndimage.filters import gaussian_filter
 
 
 class GameOfLife():
 
     def __init__(self):
-        self.width = 8
-        self.height = 8
-        self.matrix = np.zeros((self.width, self.height))
-        self.load_start_shape()
+        self.width = 16
+        self.height = 16
+        self.blur = 1
+        self.matrix = np.zeros((self.height, self.width))
+        self.blurred_matrix = gaussian_filter(self.matrix, self.blur)
+        self.add_random_shapes()
 
     def load_start_shape(self):
         with open("gol_start_shape.txt") as f:
@@ -18,42 +22,57 @@ class GameOfLife():
         for indices in shapelist:
             self.matrix[int(indices[0]), int(indices[1])] = 1
 
+    def add_random_shapes(self):
+        r_pentomino = [[0, 1], [0, 2], [1, 0], [1, 1], [2, 1]]
+        minus = [[0, 0], [0, 1], [0, 2]]
+        glider = [[0, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
+
+        shapelist = [r_pentomino, minus]
+        offset = [random.randint(3, self.height-3), random.randint(3, self.width-3)]
+
+        for indices in random.choice(shapelist):
+            self.matrix[indices[0]+offset[0], indices[1]+offset[1]] = \
+                self.matrix[indices[0]+offset[0], indices[1]+offset[1]] + 1
+
+    def check_live_or_die(self):
+
+        will_be_born = []
+        will_die = []
+
+        for loc, cell in np.ndenumerate(self.matrix):
+            neighbors = self.matrix[loc[0] - 1:loc[0] + 2, loc[1] - 1:loc[1] + 2]
+            count = np.sum(np.sum(neighbors, axis=0)) - cell
+            if count == 3:
+                will_be_born.append(loc)
+            elif all((count < 2, len(neighbors > 0))):
+                will_die.append(loc)
+            elif all((1 < count < 3, cell == 1)):
+                will_be_born.append(loc)
+            elif count > 3:
+                will_die.append(loc)
+
+        return will_be_born, will_die
+
     def play_game(self):
-        game_is_on = True
-        i = 0
-        while game_is_on:
-            plt.figure(i+1)
-            plt.matshow(self.matrix)
+        # game_is_on = True
+        # i = 0
+        # while game_is_on:
+        #     plt.figure(i + 1)
+        #     plt.matshow(self.blurred_matrix)
+        #     i += 1
+        shape_adder = random.randint(0, 1)
+        live, die = self.check_live_or_die()
+        for born in live:
+            self.matrix[born[0], born[1]] = 1
 
-            i += 1
-            will_be_born = []
-            will_die = []
+        for die in die:
+            self.matrix[die[0], die[1]] = 0
 
-            for loc, cell in np.ndenumerate(self.matrix):
-                neighbors = self.matrix[loc[0]-1:loc[0]+2, loc[1]-1:loc[1]+2]
-                print(neighbors)
-                count = np.sum(np.sum(neighbors, axis=0))-cell
-                print(count)
-                if count == 3:
-                    will_be_born.append(loc)
-                elif all((count < 2, len(neighbors > 0))):
-                    will_die.append(loc)
-                elif all((1 < count < 3, cell == 1)):
-                    will_be_born.append(loc)
-                elif count > 3:
-                    will_die.append(loc)
+        if shape_adder == 0:
+            self.add_random_shapes()
 
-            for born in will_be_born:
-                self.matrix[born[0], born[1]] = 1
+        if i > 20:
+            game_is_on = False
 
-            for die in will_die:
-                self.matrix[die[0], die[1]] = 0
-
-
-            if i > 10:
-                game_is_on = False
-        plt.show()
-
-gol = GameOfLife()
-gol.play_game()
-print("hello")
+        self.blurred_matrix = gaussian_filter(self.matrix, self.blur)
+        #plt.show()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from particles import Particle
 from bus_schedule import BusSchedule
-import colorsys
+from game_of_life import GameOfLife
 from PIL import Image, ImageOps
 import numpy as np
 from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
@@ -15,6 +15,7 @@ HIGHLIGHT_COLOR = "#8bcdcd"
 sense = SenseHat()
 sense.set_rotation(180)
 bus = BusSchedule()
+gol = GameOfLife()
 
 sense.clear()
 sense.set_pixel(0,0,0,0,255)
@@ -25,6 +26,7 @@ def load_colors():
 
     return colors
 
+
 def create_particles(num_particles):
     particle_list = []
     for _ in range(num_particles):
@@ -32,7 +34,8 @@ def create_particles(num_particles):
         
     return particle_list
 
-def draw_image(particle_list):
+
+def draw_particle_image(particle_list):
     
     for particle in particle_list:
         particle.engine()
@@ -50,6 +53,21 @@ def draw_image(particle_list):
 
     return showlist
 
+
+def draw_gol_image():
+
+    frame = gol.blurred_matrix
+    frame = frame / np.max(frame)
+
+    im = Image.fromarray(frame * 256).convert("L")
+    im = im.resize((8, 8), resample=Image.NEAREST)
+    im = ImageOps.colorize(im, black="black", mid=MIDTONE_COLOR, white=HIGHLIGHT_COLOR)
+    colorlist = list(im.getdata())
+    showlist = [list(rgb) for rgb in colorlist]
+
+    return showlist
+
+
 def particle_action(event):
     global HIGHLIGHT_COLOR
     global MIDTONE_COLOR
@@ -66,12 +84,35 @@ def particle_action(event):
         while go_rgb:
             i += 1
             #time.sleep(0.3)
-            frame = draw_image(particle_list)
+            frame = draw_particle_image(particle_list)
             sense.set_pixels(frame)
             has_pushed = sense.stick.get_events()
             if i > 10:
                 if len(has_pushed) > 0:
                        go_rgb = False
+
+
+def gol_action(event):
+    global HIGHLIGHT_COLOR
+    global MIDTONE_COLOR
+    if event.action != ACTION_RELEASED:
+        colormap = random.choice(colors)
+        HIGHLIGHT_COLOR = colormap.split(", ")[0]
+        MIDTONE_COLOR = colormap.split(", ")[1]
+
+        sense.clear()
+        i = 0
+        go_rgb = True
+        while go_rgb:
+            i += 1
+            #time.sleep(0.3)
+            frame = draw_gol_image()
+            sense.set_pixels(frame)
+            has_pushed = sense.stick.get_events()
+            if i > 10:
+                if len(has_pushed) > 0:
+                       go_rgb = False
+
 
 def bus_action(event):
     if event.action != ACTION_RELEASED:
@@ -83,5 +124,6 @@ colors = load_colors()
 sense.stick.direction_up = particle_action
 sense.stick.direction_down = bus_action
 sense.stick.direction_middle = sense.clear
+sense.stick.direction_right = gol_action
 
 pause()
